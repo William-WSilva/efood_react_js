d1c3d09edb1a02c932083f8d5f082371b59cda81
William Wsilva <williamreis.wsilva@gmail.com>
2023-08-29 18:33:02 -0300

    Correção header guia Perfil

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -9,7 +9,7 @@ export const EstiloGlobal = createGlobalStyle`
     font-family: Roboto, sans-serif;
     font-weight: normal;
     box-sizing: border-box;
-    border: 1px solid gray;
+    // border: 1px solid gray;
   }
 `
 export const TextBig = styled.h2`
